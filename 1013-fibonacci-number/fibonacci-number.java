0fca1727cff2c8eef16202a81ce7c3d72cf7579b
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-12-04 10:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (22.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -13,12 +13,27 @@ class Solution {
 
         // 
         // Bottom up:
+        // if(n<=1) return n;
+        // dp[0]=0;
+        // dp[1]=1;
+        // for(int i=2; i<=n; i++){
+        //     dp[i]=dp[i-1]+ dp[i-2];
+        // }
+        // return dp[n];
+    
+        //basic:
+        int p1=0;
+        int p2=1;
         if(n<=1) return n;
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]+ dp[i-2];
+        int curr=0;
+
+        for(int i=2; i<=n ;i++){
+            curr = p2+p1;
+            p1=p2;
+            p2=curr;
+            
         }
-        return dp[n];
+        return curr;
+    
     }
 }
