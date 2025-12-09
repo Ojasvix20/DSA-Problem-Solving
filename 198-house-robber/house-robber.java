5f4ad5b879064d64b56330d7e291c6a40fd32bc2
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-12-09 09:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (30.20%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,15 +2,14 @@ class Solution {
     static int[] dp;
     static int[] nums;
 
-    public int rob(int[] arr) {
-        nums = arr;
-        dp = new int[arr.length];
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
         Arrays.fill(dp, -1);
 
-        return solve(0); // start from 0
+        return solve(0,nums); // start from 0
     }
 
-    private static int solve(int i) {
+    private static int solve(int i, int[] nums) {
         if (i >= nums.length)
             return 0;
 
@@ -18,10 +17,10 @@ class Solution {
             return dp[i];
 
         // choice1 : +2
-        int rob = nums[i] + solve(i + 2);
+        int rob = nums[i] + solve(i + 2, nums);
 
         // Choice 2: Skip  to next
-        int skip = solve(i + 1);
+        int skip = solve(i + 1, nums);
 
         return dp[i] = Math.max(rob, skip);
     }
