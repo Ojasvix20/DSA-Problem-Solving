856bd5cc7e36c50cf958cc11d23ee0e8aac37cf4
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-12-09 11:43:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (25.20%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,25 +1,43 @@
 class Solution {
     static int[][] dp;
+
     public int uniquePaths(int m, int n) {
         dp = new int[m][n];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-        return solve(m,n,0,0);
-        
+        // for (int[] row : dp)
+        //     Arrays.fill(row, -1);
+        // return solve(m, n, 0, 0);
+
+        return solve(m,n);
     }
-    private static int solve(int m, int n, int i, int j){
-        if(i==m-1 && j==n-1){
-            return 1 ;
+
+    // private static int solve(int m, int n, int i, int j) {
+    //     if (i == m - 1 && j == n - 1) {
+    //         return 1;
+    //     }
+
+    //     if (i >= m || j >= n)
+    //         return 0;
+    //     if (dp[i][j] != -1) {
+    //         return dp[i][j];
+    //     }
+    //     int right = solve(m, n, i, j + 1);
+    //     int down = solve(m, n, i + 1, j);
+    //     return dp[i][j] = right + down;
+    // }
+
+    // Tabulation - bottom up
+    private static int solve(int m, int n) {
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
         }
 
-        if (i >= m || j >= n) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int right = solve(m, n, i, j + 1);
-        int down  = solve(m, n, i + 1, j);
-        return dp[i][j] = right + down;
+        return dp[m - 1][n - 1];
     }
 }
+
+// 
