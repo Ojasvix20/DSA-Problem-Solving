ac6f4b3593f89673ef9539ca8ec1b51e84af052e
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-12-17 09:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (59.10%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,36 +1,71 @@
 class Solution {
-    static int[][]dp;
+    static int[][] dp;
+    static int m;
+    static int n;
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
         dp = new int[m][n];
-        for(int[] i : dp){
-            Arrays.fill(i,-1);
-        }
+        // for(int[] i : dp){
+        //     Arrays.fill(i,-1);
+        // }
 
-        return countWays(obstacleGrid, 0, 0);
+        // return countWays(obstacleGrid, 0, 0);
+        return solve(obstacleGrid);
     }
 
-    static int countWays(int[][] maze, int r, int c) {
+    // static int countWays(int[][] maze, int r, int c) {
 
-        // Out of bounds i.e. no path
-        if (r >= maze.length || c >= maze[0].length) return 0;
+    //     // Out of bounds i.e. no path
+    //     if (r >= m || c >= n) return 0;
 
-        // If cell has obstacle = no path
-        if (maze[r][c] == 1) return 0;
+    //     // If cell has obstacle = no path
+    //     if (maze[r][c] == 1) return 0;
 
-        // Reached destination 
-        if (r == maze.length - 1 && c == maze[0].length - 1) return 1;
+    //     // Reached destination 
+    //     if (r == m - 1 && c == n) return 1;
 
-        if(dp[r][c]!=-1) return dp[r][c];
+    //     if(dp[r][c]!=-1) return dp[r][c];
 
-        // Move Down
-        int down = countWays(maze, r + 1, c);
+    //     // Move Down
+    //     int down = countWays(maze, r + 1, c);
 
-        // Move Right
-        int right = countWays(maze, r, c + 1);
+    //     // Move Right
+    //     int right = countWays(maze, r, c + 1);
 
-        return dp[r][c] = down + right;
+    //     return dp[r][c] = down + right;
+    // }
+    // Tabulation - bottom up
+    private static int solve(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+        for (int j = 1; j < n; j++){
+            if(obstacleGrid[0][j]==1){
+                dp[0][j]=0;
+            }else{
+                dp[0][j] = dp[0][j - 1];;
+            }
+        }
+        for (int i = 1; i < m; i++){
+            if(obstacleGrid[i][0]==1){
+                dp[i][0]=0;
+            }
+            else{
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+
+
+        for(int i = 1;i<m;i++){
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 }
