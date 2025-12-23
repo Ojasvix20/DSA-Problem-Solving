d8447994f09b7a2be75452afaab2dd97e3ce1bb7
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-12-23 09:55:54 +0530

    Time: 21 ms (56.59%) | Memory: 55.9 MB (6.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -7,26 +7,46 @@ class Solution {
         m = text1.length();
         n = text2.length();
 
-        dp = new int[m][n];
-        for(int[] i: dp){
-            Arrays.fill(i, -1);
-        }
-        return solve(text1, text2, 0, 0);
+        // dp = new int[m][n];
+        // for(int[] i: dp){
+        //     Arrays.fill(i, -1);
+        // }
+        // return solve(text1, text2, 0, 0);
+
+        // for tabulation
+        dp = new int[m+1][n+1];
+        return solve(text1, text2);
     }
 
-    private static int solve(String text1, String text2, int i, int j) {
-        if (i == text1.length() || j == text2.length()) {
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
+//// TLE FOR RECURSSIN so we optimise with 2d dp (memo)
+    // private static int solve(String text1, String text2, int i, int j) {
+    //     if (i == text1.length() || j == text2.length()) {
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        if (text1.charAt(i) == text2.charAt(j)) {
+    //     if (text1.charAt(i) == text2.charAt(j)) {
 
-            dp[i][j] =  1 + solve(text1, text2, i + 1, j + 1);
-        } else {
-            dp[i][j] =  Math.max(solve(text1, text2, i + 1, j), solve(text1, text2, i, j + 1));
+    //         dp[i][j] =  1 + solve(text1, text2, i + 1, j + 1);
+    //     } else {
+    //         dp[i][j] =  Math.max(solve(text1, text2, i + 1, j), solve(text1, text2, i, j + 1));
+    //     }
+    //     return dp[i][j];
+    // }
+
+
+    private static int solve(String text1, String text2) {
+
+        for(int i=1; i<=m;i++){
+            for(int j=1; j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j] = 1+  dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        return dp[i][j];
+        return dp[m][n];
+
     }
 }
-// TLE FOR RECURSSIN so we optimise with 2d dp (memo)
