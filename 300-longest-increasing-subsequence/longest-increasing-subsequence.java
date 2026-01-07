62142387cdbc09aaf054377a445ed34cf3851ec7
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-01-07 15:31:07 +0530

    Time: 70 ms (28.07%) | Memory: 249.2 MB (5.48%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -4,42 +4,22 @@ class Solution {
 
     public int lengthOfLIS(int[] nums) {
         n = nums.length;
-        dp = new int[n+1][n+1];
+        dp = new int[n + 1][n + 1];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        // for (int[] row : dp) {
+        //     Arrays.fill(row, -1);
+        // }
 
-        return solve(0,-1,nums);
+        // return solve(0,-1,nums);
+        return solve(nums);
     }
 
     // TOP DOWN
 
-    private static int solve(int i, int prev, int[] nums) {
-        if(i>=n) return 0;
-
-        if(prev!=-1 && dp[i][prev]!=-1) return dp[i][prev];
-
-
-        int notTake = solve(i + 1, prev, nums);
-
-        // Option 2: take (only if increasing)
-        int take = 0;
-        if (prev == -1 || nums[i] > nums[prev]) {
-            take = 1 + solve(i + 1, i, nums);
-        }
-        if(prev!=-1){
-        return dp[i][prev] = Math.max(take, notTake);
-
-        }
-        return Math.max(take, notTake);
-    }
-    // BOTTOM UP
     // private static int solve(int i, int prev, int[] nums) {
     //     if(i>=n) return 0;
 
-    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-
+    //     if(prev!=-1 && dp[i][prev]!=-1) return dp[i][prev];
 
     //     int notTake = solve(i + 1, prev, nums);
 
@@ -48,7 +28,27 @@ class Solution {
     //     if (prev == -1 || nums[i] > nums[prev]) {
     //         take = 1 + solve(i + 1, i, nums);
     //     }
+    //     if(prev!=-1){
+    //     return dp[i][prev] = Math.max(take, notTake);
 
-    //     return dp[i][prev+1] = Math.max(take, notTake);
+    //     }
+    //     return Math.max(take, notTake);
     // }
+
+    // BOTTOM UP
+    private static int solve(int[] nums) {
+        int dp[] = new int[n];
+        Arrays.fill(dp, 1);
+
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                ans = Math.max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
 }
