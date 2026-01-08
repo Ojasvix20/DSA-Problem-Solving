a6948e0eacc12467d923c5bc7f9561e729255c2f
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-01-08 11:38:02 +0530

    Time: 44 ms (11.59%) | Memory: 96.1 MB (5.24%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -4,26 +4,44 @@ class Solution {
 
     public int findLongestChain(int[][] pairs) {
         n = pairs.length;
-        dp = new int[n ][n + 1];
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
+        dp = new int[n+1 ][n + 1];
+        // for (int[] row : dp)
+        //     Arrays.fill(row, -1);
         Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
 
-        return solve(0, -1, pairs);
+        // return solve(0, -1, pairs);
+        return solve(pairs);
     }
 
-    public static int solve(int i, int prev, int[][] pairs) {
-        if (i >= n)
-            return 0;
+    // public static int solve(int i, int prev, int[][] pairs) {
+    //     if (i >= n)
+    //         return 0;
 
-        if (dp[i][prev+1] != -1)
-            return dp[i][prev+1];
+    //     if (dp[i][prev+1] != -1)
+    //         return dp[i][prev+1];
 
-        int notTake = solve(i + 1, prev, pairs);
-        int take = 0;
-        if (prev == -1 || pairs[i][0] > pairs[prev][1]) {
-            take = 1 + solve(i + 1, i, pairs);
+    //     int notTake = solve(i + 1, prev, pairs);
+    //     int take = 0;
+    //     if (prev == -1 || pairs[i][0] > pairs[prev][1]) {
+    //         take = 1 + solve(i + 1, i, pairs);
+    //     }
+    //     return dp[i][prev+1] = Math.max(take, notTake);
+    // }
+
+    public static int solve(int[][] pairs){
+        
+
+        for(int i=n-1; i>=0; i--){
+            for(int p = i-1; p>=-1; p--){
+                int notTake = dp[i+1][p+1];
+                int take = 0;
+                if(p==-1 || pairs[p][1]< pairs[i][0]){
+                    take = 1+ dp[i+1][i+1];
+                }
+                dp[i][p+1] = Math.max(take, notTake);
+
+            }
         }
-        return dp[i][prev+1] = Math.max(take, notTake);
+        return dp[0][0];
     }
 }
