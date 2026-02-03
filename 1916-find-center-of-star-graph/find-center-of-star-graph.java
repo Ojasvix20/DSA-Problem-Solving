3d710a7bceb6b0521890478e3334dbdf18dbcdcc
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-02-03 15:07:55 +0530

    Time: 0 ms (100.00%) | Memory: 110.9 MB (73.01%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -60,22 +60,33 @@
 
 // indegree approach
 
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         int n=edges.length+1;
+//         int[] degree = new int[n+1];
+//         for(int i=0; i<edges.length; i++){
+//             int u=edges[i][0];
+//             int v=edges[i][1];
+
+//             degree[u]++;
+//             degree[v]++;
+//         }
+//         for(int i=1; i<=n;i++){
+//             if(degree[i]==n-1){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+// OPTIMAL APPROACH :
 class Solution {
     public int findCenter(int[][] edges) {
-        int n=edges.length+1;
-        int[] degree = new int[n+1];
-        for(int i=0; i<edges.length; i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-
-            degree[u]++;
-            degree[v]++;
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        } else {
+            return edges[0][1];
         }
-        for(int i=1; i<=n;i++){
-            if(degree[i]==n-1){
-                return i;
-            }
-        }
-        return -1;
     }
 }
