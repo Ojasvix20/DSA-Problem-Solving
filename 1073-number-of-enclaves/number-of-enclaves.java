a7a970b0ce21804360a961b6ef9e028fc3c78a29
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-02-11 14:59:02 +0530

    Time: 8 ms (72.12%) | Memory: 61.6 MB (25.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,38 +1,58 @@
 class Solution {
-    static int m;
-    static int n;
+
+    int m, n;
+    int total;
+
+    int[] dr = { 1, -1, 0, 0 };
+    int[] dc = { 0, 0, 1, -1 };
+
     public int numEnclaves(int[][] grid) {
-        m= grid.length;
-        n= grid[0].length;
-        int count=0
-        ;
-        for(int i=0; i<m; i++){
-            if(grid[i][0]==1) dfs(i,0,grid);
-            if(grid[i][n-1]==1) dfs(i,n-1,grid);
-        }
-        for(int j=0; j<n; j++) {
-            if(grid[0][j] == 1)dfs(0, j, grid);
-            if(grid[m-1][j] == 1)dfs(m-1, j, grid);
-        }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]==1){
-                   count++;
-                }
+        m = grid.length;
+        n = grid[0].length;
+        total = 0;
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
+                    total += 1;
             }
         }
-        return count;
 
-    }
-    int[] dRow = {0, 1, 0, -1};
-    int[] dCol = {-1, 0, 1, 0};
-    public void dfs(int i, int j, int[][] grid){
-        grid[i][j]=2;
-        for(int k=0; k<4; k++){
-            int row = i+dRow[k];
-            int col = j+dCol[k];
-            if (row < 0 || col < 0 || row >= m || col >= n ||  grid[row][col]!=1)continue;
-            dfs(row, col, grid);
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1 && !visited[i][0])
+                dfs(i, 0, grid, visited);
+
+            if (grid[i][n - 1] == 1 && !visited[i][n - 1])
+                dfs(i, n - 1, grid, visited);
         }
+        for (int j = 0; j < n; j++) {
+            if (grid[0][j] == 1 && !visited[0][j])
+                dfs(0, j, grid, visited);
+            if (grid[m - 1][j] == 1 && !visited[m - 1][j])
+                dfs(m - 1, j, grid, visited);
+        }
+
+        return total;
+    }
+
+    private void dfs(int r, int c, int[][] grid, boolean[][] visited) {
+        if (grid[r][c] == 0 || visited[r][c])
+            return;
+        visited[r][c] = true;
+        total -= 1;
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if (isValid(nr, nc)) {
+                dfs(nr, nc, grid, visited);
+            }
+        }
+    }
+
+    private boolean isValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < m && y < n;
     }
 }
