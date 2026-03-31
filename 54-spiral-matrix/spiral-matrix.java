533736eadfbea188c6f53ea4100aab0e33f645a0
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-03-31 17:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (93.14%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,31 +1,50 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int top = 0, bottom = matrix.length - 1;
-        int left = 0, right = matrix[0].length - 1;
-        List<Integer> spiral = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0, bottom = n - 1, left = 0, right = m - 1;
 
-        while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++)
-                spiral.add(matrix[top][i]);
-            top++;
+        List<Integer> ans = new ArrayList<>();
 
-            for (int j = top; j <= bottom; j++)
-                spiral.add(matrix[j][right]);
-            right--;
+        int dir = 0;
+        // dir-0: left to right
+        // dir-1: top to bottom
+        // dir-2: right to left
+        // dir-3: bottom to up
 
-            if (top <= bottom) {
-                for (int k = right; k >= left; k--)
-                    spiral.add(matrix[bottom][k]);
+        while(top<=bottom && left<=right) {
+
+            if (dir == 0) {
+                //left to tright
+                for (int j = left; j <= right; j++) {
+                    ans.add(matrix[top][j]);
+                }
+                top++;
+            }
+            else if (dir == 1) {
+                // top to bottom
+                for (int j = top; j <= bottom; j++) {
+                    ans.add(matrix[j][right]);
+                }
+                right--;
+            }
+            else if (dir == 2) {
+                // right to left
+                for (int j = right; j >= left; j--) {
+                    ans.add(matrix[bottom][j]);
+                }
                 bottom--;
             }
-
-            if (left <= right) {
-                for (int l = bottom; l >= top; l--)
-                    spiral.add(matrix[l][left]);
+            else if (dir == 3) {
+                // bottom to up
+                for (int j = bottom; j >= top; j--) {
+                    ans.add(matrix[j][left]);
+                }
                 left++;
             }
+            dir = (dir+1)%4;
         }
 
-        return spiral;
+        return ans;
     }
 }
