2f02f7532df68a65dbcecaf64c2d836fec56d14b
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-04-29 14:51:10 +0530

    Time: 1 ms (4.28%) | Memory: 42.4 MB (98.46%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+        collect(root, list);
+        update(root, list);
+
+        return root;
+    }
+
+    public void collect(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+        list.add(root.val);
+        collect(root.left, list);
+        collect(root.right, list);
+
+    }
+
+    public void update(TreeNode root, List<Integer> list) {
+
+        if (root == null)
+            return;
+
+        int sum = 0;
+        for (int val : list) {
+            if (val > root.val) {
+                sum += val;
+            }
+        }
+
+        root.val += sum;
+
+        update(root.left, list);
+        update(root.right, list);
+    }
+
+}
