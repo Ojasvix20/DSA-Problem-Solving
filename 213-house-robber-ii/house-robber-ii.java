219d90955a4e633687437633ad245f0809870313
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-05-21 22:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.19%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,33 +1,24 @@
-// if we start from 0th index house, we go upto the second last house.
-// if we start from 1st index house, we go upto the last house.
 class Solution {
-    static int[] dp1;
-    static int[] dp2;
+    static int dp[];
     public int rob(int[] nums) {
-        int n= nums.length;
-        if(n==1) return nums[0];
-        dp1=new int[nums.length];
-        dp2=new int[nums.length];
-        Arrays.fill(dp1, -1);
-        Arrays.fill(dp2, -1);
-        
-        //take 0th house, and dont take last house
-        int op1 = solve(0, n-2, nums, dp1);
-            //dont take 0th house, take afterwards
-        int op2 = solve(1, n-1, nums, dp2);
-
-        return Math.max(op1, op2);
+        int n = nums.length;
+        dp = new int[n]; 
+        if(n==1){
+            return nums[0];
+        }   
+        return Math.max(solve(1,n-1,nums),solve(0,n-2,nums));
     }
-    private static int solve(int i, int last, int[] nums, int[] dp){
-        if(i>last) return 0;
-
-        if(dp[i]!=-1) return dp[i];
-
-        int skip= solve(i+1, last, nums, dp);
-
-        // take ith house
-        int take= nums[i] + solve(i+2, last, nums, dp);
-
-        return dp[i] = Math.max(take, skip);
+    public int solve(int start, int end, int nums[]){
+        if(start>=nums.length){
+            return 0;
+        }
+        if(start+1<=end){
+            dp[start+1] = Math.max(nums[start],nums[start+1]);
+        }
+        dp[start] = nums[start];
+        for(int i=2;i<=end;i++){
+            dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[end];
     }
 }
