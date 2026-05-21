228f3141d6ec1ef97d275ba17e3df2a3099717c4
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-05-21 21:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (55.15%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,70 +1,16 @@
 class Solution {
-    static int[] dp;
-    static int[] nums;
-
+    static Integer dp[];
     public int rob(int[] nums) {
-        dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-
-        // return solve(0, nums); // start from 0
-        return solve( nums); // start from 0
-    }
-
-    // TOP DOWN: 
-    // private static int solve(int i, int[] nums) {
-    //     if (i >= nums.length)
-    //         return 0;
-
-    //     if (dp[i] != -1)
-    //         return dp[i];
-
-    //     // choice1 : +2
-    //     int rob = nums[i] + solve(i + 2, nums);
-
-    //     // Choice 2: Skip  to next
-    //     int skip = solve(i + 1, nums);
-
-    //     return dp[i] = Math.max(rob, skip);
-    // }
-
-    // BOTTOM UP
-    // private static int solve(int[] nums) {
-    //     int n = nums.length;
-
-    //     if (n == 1)
-    //         return nums[0];
-
-    //     int[] dp = new int[n];
-
-    //     dp[0] = nums[0];
-    //     dp[1] = Math.max(nums[0], nums[1]);
-
-    //     for (int i = 2; i < n; i++) {
-    //         dp[i] = Math.max(
-    //                 dp[i - 1], // skip curr house
-    //                 nums[i] + dp[i - 2] // rob curr house
-    //         );
-    //     }
-
-    //     return dp[n - 1];
-    // }
-
-    // Space Opti : 
-    private static int solve(int[] nums) {
-        int prev2 = 0; // dp[i-2]
-        int prev1 = 0; // dp[i-1]
-
-        for (int num : nums) {
-            int pick = num + prev2;
-            int skip = prev1;
-
-            int curr = Math.max(pick, skip);
-
-            prev2 = prev1;
-            prev1 = curr;
+        int n = nums.length;
+        dp = new Integer[n];
+        if(n==1){
+            return nums[0];
         }
-
-        return prev1;
-    
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1],nums[0]);
+        for(int i=2;i<nums.length;i++){
+            dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];          
     }
 }
