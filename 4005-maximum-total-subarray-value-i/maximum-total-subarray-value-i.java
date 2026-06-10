9d5328ed11e03635f1e3797134a4d7c51834e1c0
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2026-06-10 13:54:44 +0530

    Time: 1 ms (100.00%) | Memory: 62.6 MB (10.29%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -1,13 +1,13 @@
 class Solution {
     public long maxTotalValue(int[] nums, int k) {
-        long mx = 0;
-        long mn = Integer.MAX_VALUE;
+        long max = 0;
+        long min = Integer.MAX_VALUE;
 
         for (int num : nums) {
-            if (num > mx) mx = num;
-            if (num < mn) mn = num;
+            if (num > max) max = num;
+            if (num < min) min = num;
         }
 
-        return (mx - mn) * 1L * k;
+        return (long)(max - min) * k;
     }
 }
